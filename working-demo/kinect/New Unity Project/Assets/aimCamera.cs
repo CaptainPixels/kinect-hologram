2229d9e36df492bcfd96bb63ec41bbeebed9cb44
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2018-03-18 17:57:36 -0600

    adding working demo we showed the judges

---
+++ working-demo/kinect/New Unity Project/Assets/aimCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimCamera : MonoBehaviour {
+
+    public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 upVector = new Vector3(0.0f, 1.0f, 0.0f);
+
+        Vector3 CubePosition = GameObject.Find("Cube").transform.position;
+        transform.LookAt(CubePosition, upVector);
+	}
+}
---
+++ working-demo/kinect/New Unity Project/Assets/transformCamera.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using System;
+
+public class transformCamera : MonoBehaviour
+{
+
+    private FileInfo theSourceFile = null;
+    private StreamReader reader = null;
+    private string text = " ";
+    public TextAsset textFile;
+    public string textLine;
+    private int count = 0;
+    public float scaleFactor = 1000;
+
+    private void Start()
+    {
+        //textLine = textFile.text;
+        //var inStream = new FileStream(@"Assets\\coords.txt", FileMode.Open, FileAccess.Read);
+        //Debug.Log(inStream);
+        string path = @"C:\Users\Mykayla\Desktop\transforms\coords_0.txt";
+        Debug.Log(path);
+        if (File.Exists(path))
+        {
+            Debug.Log("found file");
+        }
+
+        string[] stringArray = File.ReadAllLines(@"C:\Users\Mykayla\Desktop\transforms\coords_" + count + ".txt");
+        //string[] stringArray = Regex.Split(textLine, "\r\n");
+        foreach (string s in stringArray)
+        {
+            Debug.Log(s);
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var inStream = new FileStream(@"Assets\\coords_" + count + ".txt", FileMode.Open, FileAccess.Read);
+        if (File.Exists(@"C:\Users\Mykayla\Desktop\transforms\coords_" + count + ".txt"))
+        {
+            string[] stringArray = System.IO.File.ReadAllLines(@"C:\Users\Mykayla\Desktop\transforms\coords_" + count + ".txt");
+            //string[] stringArray = Regex.Split(textLine, "\r\n");
+            //foreach (string s in stringArray)
+            //{
+            //    Debug.Log(s);
+            //}
+            float x = float.Parse(stringArray[0]);
+            float y = float.Parse(stringArray[1]);
+            float z = float.Parse(stringArray[2]);
+
+            Debug.Log("x: " + x + " y: " + y + " z: " + z);
+
+            // Scale values up
+            x = x * scaleFactor;
+            y = y * scaleFactor;
+            z = z * scaleFactor;
+
+            this.transform.position = new Vector3(x, y, z);
+            //this.transform.worldToLocalMatrix(x, y, z);
+
+            File.Delete(@"C:\Users\Mykayla\Desktop\transforms\coords_" + count + ".txt");
+            //File.Delete(@"Assets\coords_" + count + ".txt");
+            count++;
+        }
+        else
+        {
+            Debug.Log(@"cant find C:\Users\Mykayla\Desktop\transforms\coords_" + count + ".txt");
+        }
+        
+        
+    }
+    
+}
